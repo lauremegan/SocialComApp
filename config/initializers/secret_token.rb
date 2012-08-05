641725883c466b754ae9dc838c783f29455d9b1a
lauremegan <lauremegan@hotmail.com>
2012-08-05 17:00:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialComApp::Application.config.secret_token = 'f64f7b51070154dcfac16e89c6d19b74e5add11b90542c30e5b22acca8ad52081db45398f5584471660afbcb4236f12c4f4c4b31f853aa8d095a1410f1338d56'
